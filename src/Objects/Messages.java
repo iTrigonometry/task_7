cd42879a6a7373040a35fcb940e5b68867522b10
Trigonometry <macdevzenit@gmail.com>
2019-02-21 19:04:46 +0300

    TODO list in class Messages add new punkti

--- src/Objects/Messages.java
+++ src/Objects/Messages.java
@@ -5,16 +5,26 @@ package Objects;
 //TODO вывод сообщений из стека когда этого захочет пользователь
 //TODO сделать создание файла для пользователя при регистрации
 //TODO сделать регистрацию
+//TODO определять читаемый файл в конструкторе класса
 
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Stack;
 
 public class Messages {
     private String username, pass;
-
+    private FileInputStream fis;
+    private Properties properties;
     Messages(String username, String pass) {
         this.username = username;
         this.pass = pass;
+        if(!checkAuthorithationData(this.username, this.pass)){
+            System.out.print("\nВы ввели неверные данные. Перезапустите программу и попробуйте снова.");
+            System.exit(0);
+        }
+
     }
 
     public void saveToFile(Stack stack){
@@ -33,4 +43,27 @@ public class Messages {
 
     }
 
+    private boolean checkAuthorithationData(String username, String pass) {
+        try {
+            fis = new FileInputStream("secretfiles/users.txt");
+            properties.load(fis);
+            if(pass == properties.get(username))
+                return true;
+        }catch(FileNotFoundException e){
+            System.out.print("\nФайла не существует.");
+            System.exit(0);
+        }catch(IOException e){
+            System.out.print("Непредвиденная оошибка.(не понятно зачем она лол)");
+            System.exit(0);
+        }
+        return false;
+    }
+
+    private void writerFromFile(String username, String pass) {
+        try {
+            fis = new FileInputStream("secretfiles/" + username + "data.txt");
+        }catch(FileNotFoundException e){
+            System.out.print("Данные этого пользователя кудато пропали. Обратитесь к дебилу которыый это писал");
+        }
+    }
 }
