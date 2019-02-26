7e3dc3497bb25f894ac6c7ca7f698fedd415fc63
iTrigonometry <macdevzenit@gmail.com>
2019-02-26 21:54:29 +0300

    plan for ddevelopment added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import Objects.Messages;
 
 public class Main {
     public static void main(String[] args){
-        Messages mess = new Messages("admin", "qwerty");
-        mess.generateRandomMessagess();
-        mess.writerFromFile();
+
     }
 }
